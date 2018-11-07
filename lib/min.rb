50c7471c6b79133d81df7f45e719c8c95bd948e1
michaelstsnow <michael.st.snow@gmail.com>
2018-11-07 13:37:07 +0000

    automatically backed up by learn

--- lib/min.rb
+++ lib/min.rb
@@ -1,5 +1,5 @@
 require 'pry'
-require_relative 'values.rb'
+require 'values.rb'
 #This is the groceries hash we'll be passing in to the get_the_min method
 # groceries = {
 #  dairy: ["milk", "yogurt", "cheese"],
